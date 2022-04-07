1c99f736cb0bf986b11b8b78cb872470f0e74c55
Daniel La Vista <daniel.j.lavista@gmail.com>
2022-04-07 06:23:16 -0700

    Lesson 1.6

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,15 +1,32 @@
-const buttonElement = document.querySelector('button');
-const inputElement = document.querySelector('input');
-const listElement = document.querySelector('ul');
-
-function addGoal() {
-    const inputValue = inputElement.value;
-    const listItemElement = document.createElement('li');
-    listItemElement.textContent = inputValue;
-    if (inputValue != '') {
-        listElement.appendChild(listItemElement);
+//Define the desired end result and the data we need, leave the rest up to Vue
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
     }
-    inputElement.value = '';
-}
+}).mount('#app');
+//Imperative approach. Define each step, step by step, to execute the desired behaviour
 
-buttonElement.addEventListener('click', addGoal);
+// const buttonElement = document.querySelector('button');
+// const inputElement = document.querySelector('input');
+// const listElement = document.querySelector('ul');
+
+// function addGoal() {
+//     const inputValue = inputElement.value;
+//     const listItemElement = document.createElement('li');
+//     listItemElement.textContent = inputValue;
+//     if (inputValue != '') {
+//         listElement.appendChild(listItemElement);
+//     }
+//     inputElement.value = '';
+// }
+
+// buttonElement.addEventListener('click', addGoal);
