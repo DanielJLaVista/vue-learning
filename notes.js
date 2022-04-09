8236b5fd92ccd7510a859d11eff86949bc709918
Daniel La Vista <daniel.j.lavista@gmail.com>
2022-04-09 04:41:17 -0700

    Starting assignment 1

--- notes.js
+++ notes.js
@@ -1,10 +1,11 @@
 //Create Vue app:
 const app = Vue.createApp();
 
-//mount app
+//connect vue to html
 app.mount("css-selector");
 //e.g.
 app.mount("#div-id");
+//selector should be unique as each vue app can only be connected to one html element
 
 //vue special props
 //on event
@@ -15,3 +16,6 @@ v-for
 
 //interpolation
 {{ }}
+
+//v-bind
+v-bind
