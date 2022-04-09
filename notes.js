af02b4016355353cd4e099b330704ad3d5f8b69c
Daniel La Vista <daniel.j.lavista@gmail.com>
2022-04-09 04:27:56 -0700

    Finished lesson 17

---
+++ notes.js
@@ -0,0 +1,17 @@
+//Create Vue app:
+const app = Vue.createApp();
+
+//mount app
+app.mount("css-selector");
+//e.g.
+app.mount("#div-id");
+
+//vue special props
+//on event
+v-on: click
+
+//for a list
+v-for
+
+//interpolation
+{{ }}
